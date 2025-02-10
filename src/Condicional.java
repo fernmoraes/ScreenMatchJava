a53c4360b113fe9de4622a2fb58f69be3f36acab
Fernando Navajas Moraes <100091001+fernmoraes@users.noreply.github.com>
2025-02-10 16:39:56 -0300

    Corrigido Erro

--- src/Condicional.java
+++ src/Condicional.java
@@ -1,7 +1,6 @@
 public class Condicional {
 
     public static void main(String[] args) {
-
         int anoDeLancamento = 2022;
         boolean incluidoNoPlano = true;
         double notaDoFilme = 8.1;
--- src/ScreenMatch.java
+++ src/ScreenMatch.java
@@ -1,7 +1,6 @@
 public class ScreenMatch {
 
     public static void main(String[] args) {
-
         System.out.println("Esse é o Screen Match");
         System.out.println("Filme: Top Gun: Maverick");
 
