31fc9ddbcf83cb90a2eb4c2afc492170be65a845
Fernando Navajas Moraes <100091001+fernmoraes@users.noreply.github.com>
2025-02-11 18:49:38 -0300

    Criado classe While.java

---
+++ src/While.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class While {
+    public static void main(String[] args) {
+        Scanner leitura = new Scanner(System.in);
+        double mediaAvaliacao = 0;
+        double nota = 0;
+        int quantidadeAvaliacao = 0;
+
+        while (nota != -1) {
+            System.out.println("Diga sua avaliação para o filme ou -1 para encerrar ");
+            nota = leitura.nextDouble();
+            if(nota != -1){
+                mediaAvaliacao += nota;
+                quantidadeAvaliacao++;
+            }
+
+        }
+
+        System.out.println("Media de avaliação: " + mediaAvaliacao / quantidadeAvaliacao);
+    }
+}
